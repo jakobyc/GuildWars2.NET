8b54473beff79f67f485555872a140ea41891b2f
Chad <chad3281@gmail.com>
2018-09-16 17:02:06 -0400

    Added calls/unit tests for titles

--- src/GuildWars2.NET/v2/Misc/Repositories/MiscRepository.cs
+++ src/GuildWars2.NET/v2/Misc/Repositories/MiscRepository.cs
@@ -1,5 +1,6 @@
-﻿using GuildWars2.NET.Data;
-using GuildWars2.NET.v2.PvP.Entities;
+﻿using GuildWars2.NET.Builders.Endpoints;
+using GuildWars2.NET.Data;
+using GuildWars2.NET.v2.Misc.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +10,23 @@ namespace GuildWars2.NET.v2.Misc.Repositories
     public class MiscRepository : GW2Repository
     {
         public MiscRepository() : base() { }
+
+        public ICollection<string>GetTitles()
+        {
+            return Retrieve<ICollection<string>>("titles");
+        }
+
+        public ICollection<Title> GetTitles(params string[] ids)
+        {
+            IEndpointBuilder builder = new EndpointBuilder().AddEndpoint("titles")
+                                                            .AddParameter("ids", ids);
+            return Retrieve<ICollection<Title>>(builder);
+        }
+
+        public Title GetTitle(string id)
+        {
+            return Retrieve<Title>($"titles/{id}");
+        }
 
         public TokenInfo GetTokenInfo(string apiKey)
         {
--- test/GuildWars2.NET.Test/Tests/MiscTest.cs
+++ test/GuildWars2.NET.Test/Tests/MiscTest.cs
@@ -1,4 +1,5 @@
 ﻿using GuildWars2.NET.Test.Dependencies;
+using GuildWars2.NET.v2.Misc.Entities;
 using GuildWars2.NET.v2.Misc.Repositories;
 using GuildWars2.NET.v2.PvP.Entities;
 using System;
@@ -15,6 +16,26 @@ namespace GuildWars2.NET.Test.Tests
         public MiscTest(RepositoryFactory factory)
         {
             this.repository = factory.GetMiscRepository();
+        }
+
+        [Fact]
+        public void GetTitles()
+        {
+            AssertCall<string[]>(repository.GetTitles());
+        }
+
+        [Theory]
+        [InlineData("40", "90")]
+        public void GetTitlesById(params string[] ids)
+        {
+            AssertCall<Title[]>(repository.GetTitles(ids));
+        }
+
+        [Theory]
+        [InlineData("30")]
+        public void GetTitle(string id)
+        {
+            AssertCall<Title>(repository.GetTitle(id));
         }
 
         [Fact]

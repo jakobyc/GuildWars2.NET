cc1de78a06902ae32b3d2c4edb801b14eb8b1f70
Chad <chad3281@gmail.com>
2018-09-16 19:57:12 -0400

    Added calls/unit tests for raids

---
+++ src/GuildWars2.NET/v2/PvE/Entities/Raid.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuildWars2.NET.v2.PvE.Entities
+{
+    public class Raid
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("wings")]
+        public Wing[] Wings { get; set; }
+    }
+
+    public class Wing
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("events")]
+        public Event[] Events { get; set; }
+    }
+
+    public class Event
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}

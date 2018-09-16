51059463b2595ba85da7a7d822b2b5c834a8e5d1
Chad <chad3281@gmail.com>
2018-09-16 14:17:54 -0400

    Added call/unit tests for token info

---
+++ test/GuildWars2.NET.Test/Tests/MiscTest.cs
@@ -0,0 +1,26 @@
+﻿using GuildWars2.NET.Test.Dependencies;
+using GuildWars2.NET.v2.Misc.Repositories;
+using GuildWars2.NET.v2.PvP.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GuildWars2.NET.Test.Tests
+{
+    public class MiscTest : ApiTest
+    {
+        private MiscRepository repository;
+
+        public MiscTest(RepositoryFactory factory)
+        {
+            this.repository = factory.GetMiscRepository();
+        }
+
+        [Fact]
+        public void GetTokenInfo()
+        {
+            AssertCall<TokenInfo>(repository.GetTokenInfo(Config.GetApiKey()));
+        }
+    }
+}

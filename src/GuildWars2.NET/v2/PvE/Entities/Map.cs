d04b6307c4b6f4fb20d7919831ba55fb052097e0
Chad <chad3281@gmail.com>
2018-09-17 19:24:38 -0400

    Added calls/unit tests for maps

---
+++ src/GuildWars2.NET/v2/PvE/Entities/Map.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuildWars2.NET.v2.PvE.Entities
+{
+    public class Map
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("min_level")]
+        public int MinLevel { get; set; }
+
+        [JsonProperty("max_level")]
+        public int MaxLevel { get; set; }
+
+        [JsonProperty("default_floor")]
+        public int DefaultFloor { get; set; }
+
+        [JsonProperty("floors")]
+        public int[] Floors { get; set; }
+
+        [JsonProperty("region_id")]
+        public string RegionId { get; set; }
+
+        [JsonProperty("region_name")]
+        public string RegionName { get; set; }
+
+        [JsonProperty("continent_id")]
+        public string ContinentId { get; set; }
+
+        [JsonProperty("continent_name")]
+        public string ContinentName { get; set; }
+
+        [JsonProperty("map_rect")]
+        public int[][] MapRect { get; set; }
+
+        [JsonProperty("continent_rect")]
+        public int[][] ContinentRect { get; set; }
+    }
+}

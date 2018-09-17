6a4ba939a591fb5096523e4e31e86d218a4f5339
Chad <chad3281@gmail.com>
2018-09-16 21:48:42 -0400

    Added calls/unit tests for outfits

---
+++ src/GuildWars2.NET/v2/Items/Entities/Outfit.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuildWars2.NET.v2.Items.Entities
+{
+    public class Outfit
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("icon")]
+        public Uri Icon { get; set; }
+
+        [JsonProperty("unlock_items")]
+        public string[] UnlockItems { get; set; }
+    }
+}

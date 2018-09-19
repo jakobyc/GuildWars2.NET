2341b6e5c65b41b416e90868cc59201d7f2bfc04
Chad <chad3281@gmail.com>
2018-09-18 21:58:20 -0400

    Added calls/unit tests for backstory/answers and backstory/questions

---
+++ src/GuildWars2.NET/v2/PvE/Entities/BackstoryQuestion.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuildWars2.NET.v2.PvE.Entities
+{
+    public class BackstoryQuestion
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("answers")]
+        public string[] Answers { get; set; }
+
+        [JsonProperty("order")]
+        public int Order { get; set; }
+    }
+}

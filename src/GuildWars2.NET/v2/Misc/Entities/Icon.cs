8c34a1225833874dab07c1515b8e684e5c8de555
Chad <chad3281@gmail.com>
2018-09-18 21:01:18 -0400

    Added calls/unit tests for files

---
+++ src/GuildWars2.NET/v2/Misc/Entities/Icon.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuildWars2.NET.v2.Misc.Entities
+{
+    public class Icon
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("icon")]
+        public Uri IconUri { get; set; }
+    }
+}

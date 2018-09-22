8dd6e1e3ccf7c439dce76724c2976eaeb5fcbb57
Chad <chad3281@gmail.com>
2018-09-22 19:24:24 -0400

    Added async methods to MiscRepository

--- src/GuildWars2.NET/v2/Misc/Repositories/MiscRepository.cs
+++ src/GuildWars2.NET/v2/Misc/Repositories/MiscRepository.cs
@@ -4,6 +4,7 @@ using GuildWars2.NET.v2.Misc.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GuildWars2.NET.v2.Misc.Repositories
 {
@@ -78,5 +79,75 @@ namespace GuildWars2.NET.v2.Misc.Repositories
         {
             return Retrieve<TokenInfo>($"tokeninfo?access_token={apiKey}");
         }
+
+        #region Async
+        public async Task<Build> GetBuildAsync()
+        {
+            return await RetrieveAsync<Build>("build");
+        }
+
+        public async Task<ICollection<Icon>> GetAllIconsAsync()
+        {
+            IEndpointBuilder builder = new EndpointBuilder().AddEndpoint("files")
+                                                            .AddParameter("ids", "all");
+            return await RetrieveAsync<ICollection<Icon>>(builder);
+        }
+
+        public async Task<ICollection<string>> GetIconIdsAsync()
+        {
+            return await RetrieveAsync<ICollection<string>>("files");
+        }
+
+        public async Task<ICollection<Icon>> GetIconsAsync(params string[] ids)
+        {
+            IEndpointBuilder builder = new EndpointBuilder().AddEndpoint("files")
+                                                            .AddParameter("ids", ids);
+            return await RetrieveAsync<ICollection<Icon>>(builder);
+        }
+
+        public async Task<Icon> GetIconAsync(string id)
+        {
+            return await RetrieveAsync<Icon>($"files/{id}");
+        }
+
+        public async Task<ICollection<string>> GetQuagganIdsAsync()
+        {
+            return await RetrieveAsync<ICollection<string>>("quaggans");
+        }
+
+        public async Task<ICollection<QuagganIcon>> GetQuaggansAsync(params string[] ids)
+        {
+            IEndpointBuilder builder = new EndpointBuilder().AddEndpoint("quaggans")
+                                                            .AddParameter("ids", ids);
+            return await RetrieveAsync<ICollection<QuagganIcon>>(builder);
+        }
+
+        public async Task<QuagganIcon> GetQuagganAsync(string id)
+        {
+            return await RetrieveAsync<QuagganIcon>($"quaggans/{id}");
+        }
+
+        public async Task<ICollection<string>> GetTitleIdsAsync()
+        {
+            return await RetrieveAsync<ICollection<string>>("titles");
+        }
+
+        public async Task<ICollection<Title>> GetTitlesAsync(params string[] ids)
+        {
+            IEndpointBuilder builder = new EndpointBuilder().AddEndpoint("titles")
+                                                            .AddParameter("ids", ids);
+            return await RetrieveAsync<ICollection<Title>>(builder);
+        }
+
+        public async Task<Title> GetTitleAsync(string id)
+        {
+            return await RetrieveAsync<Title>($"titles/{id}");
+        }
+
+        public async Task<TokenInfo> GetTokenInfoAsync(string apiKey)
+        {
+            return await RetrieveAsync<TokenInfo>($"tokeninfo?access_token={apiKey}");
+        }
+        #endregion
     }
 }
